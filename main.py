90bf212b50e7ca859bef584e5ceef394645f3b29
paratikd481 <pratikd481@gmail.com>
2026-04-03 22:37:39 +0530

    Tested query with different documents

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from app.services.vector_store_service import VectorStoreService
 service = VectorStoreService()
 
 # simulate usage
-service.add_documents(["hello world Tesst"], ["2"])
-
-result = service.query("hello")
+service.add_documents(["Mobile under 50k"], ["5"])
+service.add_documents(["Mobile under 20k"], ["6"])
+service.add_documents(["Tablet under 20k"], ["7"])
+service.add_documents(["Tablet under 20k"], ["8"])
+service.add_documents(["Laptop under 20k"], ["9"])
+result = service.query("Pc cheap")
 print(result)
